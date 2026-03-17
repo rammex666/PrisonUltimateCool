981c76f8be317728e7ce9850792613edef4237d5
.rammex <104463176+rammex666@users.noreply.github.com>
2026-03-17 13:48:17 +0100

    Remove GUI Fast Creation IDEA

--- src/main/java/fr/rammex/prisonUltimateCool/util/ItemBuilder.java
+++ src/main/java/fr/rammex/prisonUltimateCool/util/ItemBuilder.java
@@ -1,6 +1,5 @@
 package fr.rammex.prisonUltimateCool.util;
 
-import fr.rammex.prisonUltimateCool.models.ItemPage;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -10,7 +9,7 @@ import java.util.List;
 public class ItemBuilder {
 
     // TODO : adapter avec le InventoryPage
-    public void createItemForInventory(String name, String id, int slot, int amount, Material material, List<String> lore){
+    public ItemStack createItemForInventory(String name, String id, int slot, int amount, Material material, List<String> lore){
         ItemStack item = new ItemStack(material, amount);
         ItemMeta itemMeta = item.getItemMeta();
 
@@ -19,7 +18,6 @@ public class ItemBuilder {
 
         item.setItemMeta(itemMeta);
 
-        ItemPage itemPage = new ItemPage(id,item,slot);
-
+        return item;
     }
 }
--- src/main/java/fr/rammex/prisonUltimateCool/util/PageBuilder.java
+++
@@ -1,5 +0,0 @@
-package fr.rammex.prisonUltimateCool.util;
-
-public class PageBuilder {
-
-}
